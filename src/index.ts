61b780e350d24a45fe2f47525aefc2b8483b38b8
Kohei Omachi <kohei.omachi@gmail.com>
2024-04-13 18:44:32 +0900

    chore: gha

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = new Hono<{ Bindings: Bindings }>()
 figlet.parseFont("Standard", standard);
 
 app.get('/', (c) => {
-const body = figlet.textSync(`Bun ${c.env.ENV}!`);
+const body = figlet.textSync(`Bun ${c.env.ENV} GitHub Actions!`);
   return c.text(body)
 })
 
